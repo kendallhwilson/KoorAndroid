154870b6c04417d602d7e90cfd8105d7b4ae33ca
kendallhwilson <kendallhwilson@gmail.com>
2014-11-01 09:06:50 -0700

    Bidding display code

--- RookAndroid/src/com/plnu/koorgame/BidFragment.java
+++ RookAndroid/src/com/plnu/koorgame/BidFragment.java
@@ -4,13 +4,27 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class BidFragment extends Fragment {
+public class BidFragment extends Fragment implements OnClickListener {
+	public TextView currentHighBid;
+	public TextView playerBid;
+	private int bid = 0;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	        // Inflate the layout for this fragment
-	        return inflater.inflate(R.layout.bid_layout, container, false);
+		View v = inflater.inflate(R.layout.bid_layout, container, false);
+		currentHighBid = (TextView) v.findViewById(R.id.current_bid_number_textview);
+		playerBid = (TextView) v.findViewById(R.id.my_bid_number_textview);
+		
+		Button increaseFiveButton = (Button) v.findViewById(R.id.bid_add_five_button);
+		increaseFiveButton.setOnClickListener(this);
+		Button bidChosenButton = (Button) v.findViewById(R.id.bid_go_button);
+		bidChosenButton.setOnClickListener(this);
+		
+        return v;
 	}
 	
     @Override
@@ -23,6 +37,32 @@ public class BidFragment extends Fragment {
 	public void onPause() {
 		super.onPause();
 		
+	}
+	
+	
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.bid_add_five_button:
+    		if (bid < 200) { //check bidding rules 
+    			bid += 5;
+    			playerBid.setText(String.valueOf(bid));
+    		}
+            break;
+        case R.id.bid_go_button:
+        	//call method to send new bid
+        	break;
+        }
+    }
+	
+	/*
+	 * Method called to update high bid in GUI
+	 * @param the high bid we are showing
+	 */
+	public void displayHighBid(int highBid){
+		currentHighBid.setText(String.valueOf(highBid));
+		playerBid.setText(String.valueOf(highBid));
+		bid = highBid;
 	}
 }
 
