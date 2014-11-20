699bd48ad2f928d7720a1a325bc03f53b01a9cc8
kendallhwilson <kendallhwilson@gmail.com>
2014-11-20 12:37:44 -0800

    Revert "start round"
    This reverts commit 1782f2d469ff9e60c6c9cb190dd0d65d5bfb225d.

--- RookAndroid/src/com/plnu/koorgame/GameBoard.java
+++ RookAndroid/src/com/plnu/koorgame/GameBoard.java
@@ -29,9 +29,16 @@ public class GameBoard extends Activity implements onBidListener {
 		discardFragment = new DiscardFragment();
 		
 		game = new Game();
+		setupNewRound();
+	}
+	
+	/*
+	 * Allows us to create a new deck and restart a round if we have not reached 500 or it is the first round of the game.
+	 */
+	public void setupNewRound(){
 		game.makeDeck();
 		game.dealCards();
-		int [] bids = game.advanceBidding(); //ONLY HAVE AI PLAYERS MAKE THEIR BIDS
+		int [] bids = game.advanceBidding();
 		startBiddingFragment(bids);
 	}
 	
@@ -66,6 +73,7 @@ public class GameBoard extends Activity implements onBidListener {
 			bidFragment.displayPlayerBid(2,  bids[1]);
 			bidFragment.displayPlayerBid(3,  bids[2]);
 		}
+		//call discard fragment
 	}
 
 	/*
