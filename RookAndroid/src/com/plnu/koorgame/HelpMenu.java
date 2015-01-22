1fb6641be2a666b4e5819f4a930aace5a4608d72
kendallhwilson <kendallhwilson@gmail.com>
2015-01-22 12:07:45 -0800

    The actual new files

---
+++ RookAndroid/src/com/plnu/koorgame/HelpMenu.java
@@ -0,0 +1,14 @@
+package com.plnu.koorgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpMenu extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_layout);
+	}
+
+}
---
+++ RookAndroid/src/com/plnu/koorgame/SettingsMenu.java
@@ -0,0 +1,13 @@
+package com.plnu.koorgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_layout);
+	}
+}
