3be5514bc38817c1a07b734b0cf10bfbfcdd4e26
kendallhwilson <kendallhwilson@gmail.com>
2015-04-27 14:06:09 -0700

    New Game over fragment code

---
+++ RookAndroid/src/com/plnu/koorgame/GameOverDialogFragment.java
@@ -0,0 +1,22 @@
+package com.plnu.koorgame;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class GameOverDialogFragment extends DialogFragment {
+	
+	@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		String message = "Game Over!\n" + "Winner is " + getArguments().getString("WINNERNAME")
+				+ " with "  + getArguments().getInt("WINNERSCORE") + " points!";
+		message += "\n Loser is " + getArguments().getString("LOSERNAME") + " with " + 
+				getArguments().getInt("LOSERSCORE") + " points :( .";
+		builder.setMessage(message);
+		builder.setNeutralButton(R.string.ok, null);
+        return builder.create();
+    }
+
+}
